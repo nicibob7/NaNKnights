1d2684dd61e43368e3e4ad12f94ee898ca53e7a3
Cams-Plan <ca.francis@outlook.com>
2023-08-24 12:32:54 +0100

    suggestions done

--- tests/models/Suggestion.spec.js
+++ tests/models/Suggestion.spec.js
@@ -69,15 +69,33 @@ describe("Suggestion Models unit tests", () => {
             
         })
     })
-    // DO GETALL() METHOD
 
-    // describe("Suggestion.getAll()", () => {
+    describe("Suggestion.getAll()", () => {
 
-    //     it("Should return all suggestions with falsy image values being changed to base64", async () => {
+        it("Should return all suggestions with falsy image values being changed to base64", async () => {
+
+            const suggestion3 = {
+                id: 1,
+                title: "suggestion1",
+                description: "this is a suggestion",
+                date_posted: "2023-08-23 12:47:19.960744",
+                posted_by: "bob",
+                votes: 1,
+                is_resolved: false,
+                is_activated: false,
+                image: 1,
+                urgency_level: "low"
+            }
+
+            jest.spyOn(db,'query').mockResolvedValueOnce({rows: [suggestion1, suggestion3]});
+
+            const result = await Suggestion.getAll()
+            expect(typeof(result[1].image)).toBe('string')
+            expect(result.length).toBe(2)
 
 
-    //     })
-    // })
+        })
+    })
 
     describe("Suggestion.getSuggestionByPopularity()", () => {
 
@@ -107,7 +125,6 @@ describe("Suggestion Models unit tests", () => {
         })
     })
 
-    // THREE METHODS LEFT
     describe("Suggestion.getById(id)", () => {
 
         it("Should return all data from one suggestion that matches the id provided", async () => {
@@ -131,38 +148,74 @@ describe("Suggestion Models unit tests", () => {
     })
 
     
-    describe("Suggestion.delete(id)", () => {
+    // describe("Suggestion.delete(id)", () => {
 
-        it("If data deletion is successful, it should return undefined", async () => {
+    //     it("If data deletion is successful, it should return undefined or chosen message", async () => {
 
-            const id = 1
+    //         const id = 1
 
-            const mockQueryRes = jest.spyOn(db, 'query')
-                .mockResolvedValue({
-                    command: 'DELETE',
-                    rowCount: 0,
-                    oid: null,
-                    rows: [],
-                    fields: [],
-                    _parsers: undefined,
-                    RowCtor: null,
-                    rowAsArray: false
-                  });
+    //         const mockQueryRes = jest.spyOn(db, 'query')
+    //             .mockResolvedValue({
+    //                 command: 'DELETE',
+    //                 rowCount: 0,
+    //                 oid: null,
+    //                 rows: [],
+    //                 fields: [],
+    //                 _parsers: undefined,
+    //                 RowCtor: null,
+    //                 rowAsArray: false
+    //               });
 
-            const result = await Suggestion.delete(id)
+    //         const result = await Suggestion.delete(id)
 
-            expect(result).toBe(undefined);
-            expect(typeof(result)).not.toBe('object');
-        })
-    })
-
-    // describe("Suggestion.update(id,data)", () => {
-
-    //     it("Should return all of rows of data from the updated dataset", () => {
-
-
+    //         // expect(result).toBe(undefined);
+    //         expect(typeof(result)).not.toBe('object');
     //     })
     // })
+
+    describe("suggestionInstance.activate(id,data)", () => {
+
+        it("Should return all of rows of data from the updated dataset", async () => {
+            
+            let newSuggestion = new Suggestion(suggestion1)
+
+            const suggestion2 = {
+                id: 1,
+                title: "suggestion1",
+                description: "this is a suggestion",
+                date_posted: "2023-08-23 12:47:19.960744",
+                posted_by: "bob",
+                votes: 1,
+                is_resolved: false,
+                is_activated: true,
+                image: null,
+                urgency_level: "low"
+            }
+
+            jest.spyOn(db,'query').mockResolvedValueOnce({rows: [suggestion2]});
+
+            const result = await newSuggestion.activate()
+            expect(result.is_activated).toBe(true)
+
+        })
+
+        it("Should throw error if row of suggestions does not equal 1", async () => {
+
+            let newSuggestion = new Suggestion(suggestion1)
+
+            let error1 = new Error("Unable to locate suggestion")
+
+                jest.spyOn(db, 'query').mockRejectedValue(error1)
+
+                try {
+                    const response = await newSuggestion.activate()
+                } catch (error) {
+                    expect(error).toBeTruthy()
+                    expect(error).toBeInstanceOf(Error)
+                    expect(error.message).toBe("Unable to locate suggestion")
+                }
+        })
+    })
 
 
 })
